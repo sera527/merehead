fd3a77e8fa31a4910a543a8b9594b0b552075e45
Serhii Posternak <serpos95@gmail.com>
2018-03-16 02:28:31 +0200

    created Book model and migration

---
+++ database/migrations/2018_03_15_202102_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('author', 100);
+            $table->string('name', 100);
+            $table->unsignedInteger('number_of_pages');
+            $table->text('description');
+            $table->string('image', 150);
+            $table->unsignedInteger('owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
